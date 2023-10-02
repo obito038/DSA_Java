dfc3ae718e28b86e72470b9fb341ae78503a914d
Prasun kr Mondal <89583693+obito038@users.noreply.github.com>
2023-10-02 23:56:17 +0530

    Create SerachInsertPosition.java

---
+++ SerachInsertPosition.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public static int searchInsert(int [] arr, int m){
+        // Write your code here.
+        int n = arr.length;
+        int low = 0;
+        int high = n -1;
+        int ans = n;
+
+        while(low <= high){
+            int mid = low+(high-low)/2;
+            if( arr[mid] >= m){
+                ans = mid;
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+
+    }
+}
