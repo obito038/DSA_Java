df75a7df32c49ee46ad43dfc97042fc67d8ed825
Prasun kr Mondal <89583693+obito038@users.noreply.github.com>
2023-10-04 23:58:18 +0530

    Create MinInRotatedSortedArray.java

---
+++ MinInRotatedSortedArray.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public static int findMin(int []arr) {
+        // Write your code here.
+        int low = 0;
+        int high = arr.length - 1;
+        int ans = Integer.MAX_VALUE;
+
+        while( low <= high ){
+            int mid = low + (high - low)/2;
+            if( arr[low] <= arr[mid] ){
+                ans = Math.min(ans, arr[low]);
+                low = mid + 1;
+            }
+            else{
+                ans = Math.min(ans, arr[mid]);
+                high = mid - 1;
+            }
+
+        }
+        return ans;
+    }
+}
