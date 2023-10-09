8c3c2fb6cfc217b29521fd37cf8b40da3bd30248
Prasun kr Mondal <89583693+obito038@users.noreply.github.com>
2023-10-09 23:50:06 +0530

    Create asciiValueOfChar.java

---
+++ asciiValueOfChar.java
@@ -0,0 +1,8 @@
+public class asciiValueOfChar {
+    public static void main( String []args){
+//        char c = 'a';
+        String str = "ABC";
+        char c = str.charAt(0);
+        System.out.println((int)c);
+    }
+}
