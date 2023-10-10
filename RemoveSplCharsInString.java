a4f17d721ecd81420bdcd8b88678f80595c81a80
Prasun kr Mondal <89583693+obito038@users.noreply.github.com>
2023-10-10 19:56:02 +0530

    Update RemoveSplCharsInString.java

--- RemoveSplCharsInString.java
+++ RemoveSplCharsInString.java
@@ -8,11 +8,14 @@ public static void main(String[]  args)
 }
 }
 
-//    public static void main(String[] args) {
-//        String str = "P/ra-su^n#k:r@M<O>;nd?al";
-//        String spl = "[^a-zA-Z0-9]";
-//
-//        String newStr = str.replaceAll(spl, "");
-//
-//        System.out.println("The new string without special characters is: " + newStr);
-//    }
+// import java.util.Scanner;
+
+// public class RemoveSplChar {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter a string: ");
+//         String str = sc.nextLine();
+//         str = str.replaceAll("[^a-zA-z0-9]","");
+//         System.out.println(str);
+//     }
+// }
