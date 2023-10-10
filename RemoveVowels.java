77bfe4506a60a997d5fbc406e23fdf10d9011f94
Prasun kr Mondal <89583693+obito038@users.noreply.github.com>
2023-10-10 19:38:10 +0530

    Update RemoveVowels.java

--- RemoveVowels.java
+++ RemoveVowels.java
@@ -6,3 +6,21 @@ public class RemoveVowels {
         System.out.println(newStr);
     }
 }
+
+
+//Approach2//
+// import java.util.Scanner;
+// public class vowelremove {
+//     public static void main(String[] args) {
+// //        String str = "Prasun kr modal";
+//         Scanner sc= new Scanner(System.in);
+//         System.out.print("Enter a string: ");
+//         String str= sc.nextLine();
+//         System.out.println("You have entered: "+str);
+//         System.out.print("After removing vowels: " + vremove(str));
+//     }
+//     static String vremove(String str){
+//         str = str.replaceAll("([aeiouAEIOU])", "");
+//         return str;
+//     }
+// }
